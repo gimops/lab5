4fb9516027cdd32ff57dcbab728580092943b46e
gimops <74595367+gimops@users.noreply.github.com>
2020-11-17 15:01:11 +0500

    Add engine

---
+++ lab5/engine/connection/connection.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user = 'f0474098_user';
+$password = 'root';
+$database = 'f0474098_db';
+?>
---
+++ lab5/engine/page/title.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+	<head>
+        <?require_once 'head.php';?>
+        <title>
+            Гимаева Галия Лабораторная работа №5
+        </title>
+	</head>
+</html>
